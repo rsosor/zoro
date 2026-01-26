87dde7e50f6b0df708137d26e4cd598b03ad9b6d
Aaron <godpkpk205@gmail.com>
2026-01-27 03:38:45 +0800

    :)

--- zoro/next.config.ts
+++ zoro/next.config.ts
@@ -6,6 +6,12 @@ const nextConfig: NextConfig = {
     unoptimized: true, // GitHub Pages 不支援預設的圖片優化
   },
   /* config options here */
+  // 核心修正：加入這行
+  // 注意：開頭一定要有斜線，最後不要有斜線
+  basePath: '/zoro', 
+  
+  // 有些情況也建議加上這個，確保資產路徑正確
+  assetPrefix: '/zoro/',
 };
 
 export default nextConfig;
