56596e24e87d6e940302695a429e9ac53b8d9bca
Aaron <godpkpk205@gmail.com>
2026-01-27 03:22:16 +0800

    :)

--- zoro/next.config.ts
+++ zoro/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export'
+  output: 'export',
+  images: {
+    unoptimized: true, // GitHub Pages 不支援預設的圖片優化
+  },
   /* config options here */
 };
 
